06b911d86ec02b1532471d8b949607899aa4100f
Michael Farwell <whitewolf9909@gmail.com>
2023-05-25 15:13:24 -0400

    complete chapter 20

--- src/routes/(dashboard)/invoices/+page.svelte
+++ src/routes/(dashboard)/invoices/+page.svelte
@@ -41,9 +41,11 @@
   </div>
 
   <!--  Invoices -->
-  {#each $invoices as invoice}
-    <InvoiceRow {invoice} />
-  {/each}
+  <div class="flex flex-col-reverse">
+    {#each $invoices as invoice}
+      <InvoiceRow {invoice} />
+    {/each}
+  </div>
 </div>
 
 <CircledAmount
--- src/routes/(dashboard)/invoices/InvoiceRow.svelte
+++ src/routes/(dashboard)/invoices/InvoiceRow.svelte
@@ -1,22 +1,40 @@
 <script lang="ts">
+  import AdditionalOptions from "$lib/components/AdditionalOptions.svelte";
+  import Edit from "$lib/components/Icon/Edit.svelte";
+  import Send from "$lib/components/Icon/Send.svelte";
   import ThreeDots from "$lib/components/Icon/ThreeDots.svelte";
+  import Trash from "$lib/components/Icon/Trash.svelte";
   import View from "$lib/components/Icon/View.svelte";
   import Tag from "$lib/components/Tag.svelte";
   import { convertDate, isLate } from "$lib/utils/dateHelpers";
   import { asCurrency, sumLineItems } from "$lib/utils/moneyHelpers";
 
   export let invoice: Invoice;
+  let isAdditionalMenuShowing: boolean = false;
+  let isOptionsDisabled: boolean = false;
 
   const getInvoiceLabel = (): string => {
     if (invoice.invoiceStatus === "draft") {
       return "draft";
     } else if (invoice.invoiceStatus === "sent" && !isLate(invoice.dueDate)) {
+      isOptionsDisabled = true;
       return "sent";
     } else if (invoice.invoiceStatus === "sent" && isLate(invoice.dueDate)) {
+      isOptionsDisabled = true;
       return "late";
     } else if (invoice.invoiceStatus === "paid") {
+      isOptionsDisabled = true;
       return "paid";
     }
+  };
+  const handleDelete = () => {
+    console.log("Deleting");
+  };
+  const handleEdit = () => {
+    console.log("Editing");
+  };
+  const handleSend = () => {
+    console.log("Sending");
   };
 </script>
 
@@ -36,17 +54,27 @@
   <div class="text-sm lg:text-lg font-mono font-bold amount text-right">
     {asCurrency(sumLineItems(invoice.lineItems))}
   </div>
-  <div class="viewButton hidden text-sm lg:block lg:text-lg lg:center">
+  <div class="center viewButton hidden text-sm lg:flex lg:text-lg">
     <a
         href="#"
         class="text-pastelPurple hover:text-daisyBush">
       <View />
     </a>
   </div>
-  <div class="moreButton hidden text-sm lg:block lg:text-lg lg:center">
-    <button class="text-pastelPurple hover:text-daisyBush">
+  <div class="center moreButton hidden text-sm lg:flex lg:text-lg relative">
+    <button
+        class="text-pastelPurple hover:text-daisyBush"
+        on:click={() => isAdditionalMenuShowing = !isAdditionalMenuShowing}>
       <ThreeDots />
     </button>
+    {#if isAdditionalMenuShowing}
+      <AdditionalOptions
+          options={[
+          {label: "Edit", icon: Edit, onClick: handleEdit, disabled: isOptionsDisabled},
+          {label: "Delete", icon: Trash, onClick: handleDelete, disabled: false},
+          {label: "Send", icon: Send, onClick: handleSend, disabled: isOptionsDisabled},
+      ]} />
+    {/if}
   </div>
 </div>
 
